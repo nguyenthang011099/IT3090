2a16743a473b62d60d0246015278763d14951dc7
thang <nguyenthang011099.hust@gmail.com>
2019-10-06 16:59:53 +0700

    add file laravel

--- database/factories/CungcapFactory.php
+++ database/factories/CungcapFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Cungcap;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
-    return [
+$factory->define(Cungcap::class, function (Faker $faker) {
 
+    $p=$faker->numberBetween(10,10000);
+    return [
+        'MSMH'=>$faker->numberBetween(1000,10000),
+        'Price'=>$p.'0.000',
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       $this->call(NhacungcapSeeder::class);
+       $this->call(CungcapSeeder::class);
 
     }
 }
